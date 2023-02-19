afca2e517faab00b705a04376afec72c3d541ce5
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-02-19 16:19:57 -0300

    Atualizar 'main.ts, test.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
 namespace morse {
-    
+    export function makeLongSound(show: boolean = false) {
+        if(show == false){
+            music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        }else{
+            basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+            music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        }
+    }
+    export function makeShortSound(show: boolean = false) {
+        if (show == false) {
+            music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        } else {
+            basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+            music.playTone(Note.C, music.beat(BeatFraction.Half))
+        }
+    }
 }
+
--- test.ts
+++ test.ts
@@ -1,1 +1,2 @@
 // testes vão aqui; isto não será compilado quando este pacote for usado como uma extensão.
+morse.makeLongSound(true)
